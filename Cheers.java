d1618bedafd4812d76d19f9f05a04f524540bb2e
Ronbeker <ronbeker@gmail.com>
2024-11-20 16:51:02 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,27 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
-        }
+            String input = args[0].toUpperCase();
+            int times = Integer.parseInt(args[1]);
+            String an = "AEFHILMNOR";
+            for(int i = 0; i< input.length() ; i++)
+            {
+                char current = input.charAt(i);
+                System.out.print("Give me ");
+                if(an.indexOf(current) != -1) {
+                    System.out.print("an ");
+                }
+                else
+                {
+                    System.out.print("a ");
+                }
+                System.out.println(current + "!");
+
+            }
+            for(int i = 0; i < times; i++)
+            {
+                System.out.println(input +"!!!");
+            }
+            
+            }
 }
